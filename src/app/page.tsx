d2f1677e22b1421b2b3f2eb9e9b4217a8f7b3a44
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-08-08 08:41:14 -0700

    SkillOrbit after hero setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Hero from "@/components/common/Hero";
+import AfterHero from "@/components/common/AfterHero";
 
 export default function Home() {
   return (
+    <>
     <Hero/>
+    <AfterHero/>
+    </>
   )
 }
